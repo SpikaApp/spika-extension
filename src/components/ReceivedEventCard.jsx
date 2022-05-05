b674ab06ae6a76849a439484696b76bca09b8936
xorgal <93151521+xorgal@users.noreply.github.com>
2022-05-05 16:39:31 +0300

    improved txn tab

--- src/components/ReceivedEventCard.jsx
+++ src/components/ReceivedEventCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListItem, Divider } from "@mui/material";
+import { ListItem, Divider, Stack, Typography, Tooltip, Chip } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 import shortenAddress from "../utils/shortenAddress";
 
@@ -9,15 +9,27 @@ const ReceivedEventCard = ({
     data: { amount, from },
   },
 }) => {
+  const handleClick = () => {
+    navigator.clipboard.writeText(from);
+  };
+
   return (
     <div className="ReceivedEvent" key={sequence_number}>
       <ListItem>
-        <DownloadIcon sx={{ fontSize: 24, marginRight: 2 }} color="primary" />
-        From: {shortenAddress(from)}
-        <br />
-        Amount: {amount}
-        <br />
-        Txn No. {sequence_number}
+        <DownloadIcon sx={{ fontSize: 24, marginRight: 2, marginTop: 1 }} color="primary" />
+        <Stack>
+          <Stack sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <Typography sx={{ marginRight: 1 }}>From:</Typography>
+            <Tooltip title="Copy address">
+              <Chip label={shortenAddress(from)} onClick={handleClick} />
+            </Tooltip>
+          </Stack>
+          <Stack sx={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
+            Amount: {amount} TestCoin
+            <br />
+            Transaction No. {sequence_number}
+          </Stack>
+        </Stack>
       </ListItem>
       <Divider />
     </div>
--- src/components/SentEventCard.jsx
+++ src/components/SentEventCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListItem, Divider } from "@mui/material";
+import { ListItem, Divider, Stack, Typography, Tooltip, Chip } from "@mui/material";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import shortenAddress from "../utils/shortenAddress";
 
@@ -9,15 +9,27 @@ const SentEventCard = ({
     data: { amount, to },
   },
 }) => {
+  const handleClick = () => {
+    navigator.clipboard.writeText(to);
+  };
+
   return (
     <div className="sentEvent" key={sequence_number}>
       <ListItem>
-        <FileUploadIcon sx={{ fontSize: 24, marginRight: 2 }} color="primary" />
-        To: {shortenAddress(to)}
-        <br />
-        Amount: {amount}
-        <br />
-        Txn No. {sequence_number}
+        <FileUploadIcon sx={{ fontSize: 24, marginRight: 2, marginTop: 1 }} color="primary" />
+        <Stack>
+          <Stack sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <Typography sx={{ marginRight: 1 }}>Sent to:</Typography>
+            <Tooltip title="Copy address">
+              <Chip label={shortenAddress(to)} onClick={handleClick} />
+            </Tooltip>
+          </Stack>
+          <Stack sx={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
+            Amount: {amount} TestCoin
+            <br />
+            Transaction No. {sequence_number}
+          </Stack>
+        </Stack>
       </ListItem>
       <Divider />
     </div>
