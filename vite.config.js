35c60280d278df7ff1fc676977bb19ac465ecbe2
xorgal <93151521+xorgal@users.noreply.github.com>
2022-06-08 00:16:47 +0300

    Updated to Aptos SDK v1.0.0

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: ["es2020", "esnext"],
+    module: "commonjs",
+    target: "es2020",
+  },
+
   server: {
     proxy: {
       "/api": {
