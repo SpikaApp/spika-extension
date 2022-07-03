e670396df751f997c4d8239bac8b60338877f8a3
xorgal <93151521+xorgal@users.noreply.github.com>
2022-07-03 07:49:29 +0300

    update events to withdraw_events and deposit_events

--- src/components/ReceivedEventCard.jsx
+++ src/components/ReceivedEventCard.jsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { ListItem, Divider, Stack, Typography, Tooltip, Chip } from "@mui/material";
+import { ListItem, Divider, Stack } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
-import shortenAddress from "../utils/shortenAddress";
 
 const ReceivedEventCard = ({
   receivedEvent: {
     sequence_number,
-    data: { amount, from },
+    data: { amount },
   },
 }) => {
-  const handleClick = () => {
-    navigator.clipboard.writeText(from);
-  };
-
   return (
-    <div className="ReceivedEvent" key={sequence_number}>
+    <div className="ReceivedEvent">
       <ListItem>
         <DownloadIcon sx={{ fontSize: 24, marginRight: 2, marginTop: 1 }} color="primary" />
         <Stack>
-          <Stack sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-            <Typography sx={{ marginRight: 1 }}>From:</Typography>
-            <Tooltip title="Copy address">
-              <Chip label={shortenAddress(from)} onClick={handleClick} />
-            </Tooltip>
-          </Stack>
           <Stack sx={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
             Amount: {amount} TestCoin
             <br />
