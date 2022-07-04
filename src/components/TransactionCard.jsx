5533464de0ef16d0be1691c1cdef68a5c5594177
xorgal <93151521+xorgal@users.noreply.github.com>
2022-07-05 01:36:25 +0300

    bugfix txn tab crashing app

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { ListItem, Divider, Stack, Chip, Tooltip, Typography, Link } from "@mui/material";
+import { ListItem, Divider, Stack, Tooltip, Typography, Link } from "@mui/material";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-import shortenKey from "../utils/shortenKey";
-import shortenAddress from "../utils/shortenAddress";
 
-const TransactionCard = ({
-  transaction: {
-    gas_used,
-    version,
-    vm_status,
-    events: {
-      0: {
-        data: { amount },
-      },
-      1: { key },
-    },
-  },
-}) => {
-  const handleClick = () => {
-    navigator.clipboard.writeText(shortenKey(key));
-  };
-
+const TransactionCard = ({ transaction: { gas_used, version, status, vm_status } }) => {
   return (
     <div className="transaction">
       <ListItem>
@@ -40,21 +22,9 @@ const TransactionCard = ({
                 </Link>
               </Tooltip>
             </Stack>
-            Amount: {amount} TestCoin
-            <br />
             Gas used: {gas_used}
             <br />
-            <Stack direction="row">
-              <Typography sx={{ my: 0.5, ml: 0.1, mr: 1 }}>Recipient: </Typography>
-              <Tooltip title="Copy address">
-                <Chip
-                  sx={{ marginBottom: 1 }}
-                  label={shortenAddress(shortenKey(key))}
-                  onClick={handleClick}
-                />
-              </Tooltip>
-            </Stack>
-            <Typography sx={{ my: -1, mb: 0.5 }}>Status: {vm_status}</Typography>
+            Status: {vm_status}
           </Stack>
         </Stack>
       </ListItem>
