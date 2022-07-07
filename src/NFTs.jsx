2f4d0c055207ba32981215cb4c342f0e1656d17a
xorgal <93151521+xorgal@users.noreply.github.com>
2022-07-07 07:02:00 +0300

    add NFT view, create, create collection

--- src/NFTs.jsx
+++ src/NFTs.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Container,
   Typography,
@@ -7,15 +7,46 @@ import {
   CardActions,
   Stack,
   Button,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  CircularProgress,
 } from "@mui/material";
 import PaletteIcon from "@mui/icons-material/Palette";
 import ConstructionIcon from "@mui/icons-material/Construction";
 import { UIContext } from "./context/UIContext";
+import { AccountContext } from "./context/AccountContext";
 import CreateCollectionDialog from "./components/CreateCollectionDialog";
 import CreateNftDialog from "./components/CreateNftDialog";
+import emptyNft from "./assets/emptyNft.jpg";
 
 const NFTs = () => {
   const { handleCreateCollectionUI, handleCreateNFTUI } = useContext(UIContext);
+  const { accountImported, getAccountTokens, getNftDetails, accountTokens, nftDetails } =
+    useContext(AccountContext);
+  const [isWaiting, setIsWaiting] = useState(false);
+
+  useEffect(() => {
+    if (accountImported === true) {
+      setIsWaiting(true);
+      getAccountTokens();
+      const updateAccountResources = window.setInterval(() => {
+        getAccountTokens();
+      }, 30000);
+      return () => window.clearInterval(updateAccountResources);
+    }
+    return undefined;
+  }, [accountImported]);
+
+  useEffect(() => {
+    if (accountImported && accountTokens.length > 0) {
+      getNftDetails();
+      setIsWaiting(false);
+    } else if (accountTokens === 0) {
+      setIsWaiting(false);
+    }
+  }, [accountTokens]);
+
   return (
     <Container maxWidth="xs">
       <Typography variant="h6" align="center" color="textPrimary" gutterBottom>
@@ -24,15 +55,64 @@ const NFTs = () => {
         NFTs
       </Typography>
       <Card>
-        <CardContent>
-          <Stack direction="row" spacing={2}>
+        <CardContent sx={{ minHeight: 300 }}>
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
             <Button onClick={handleCreateCollectionUI}>Create collection</Button>
             <Button onClick={handleCreateNFTUI}>Create NFT</Button>
           </Stack>
+          {isWaiting && (
+            <Stack direction="column" sx={{ display: "flex", alignItems: "center", mt: 4 }}>
+              <Typography align="center" color="textSecondary" gutterBottom>
+                Pulling metadata...
+              </Typography>
+              <CircularProgress sx={{ mt: 4 }} color="info" />
+            </Stack>
+          )}
+          {accountTokens === 0 ? (
+            <Typography sx={{ mt: 4 }} align="center" color="textPrimary" gutterBottom>
+              No NFTs found
+            </Typography>
+          ) : (
+            <Stack sx={{ maxWidth: 350 }}>
+              <ImageList
+                gap={10}
+                variant="masonry"
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                {nftDetails.map((nft) => (
+                  <ImageListItem key={nft.name}>
+                    <img
+                      src={`${nft.uri}`}
+                      onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = emptyNft;
+                      }}
+                    />
+                    <ImageListItemBar
+                      title={<span>{nft.name}</span>}
+                      subtitle={<span>{nft.collection}</span>}
+                      position="bottom"
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+            </Stack>
+          )}
         </CardContent>
-        <CardActions>
-          <ConstructionIcon sx={{ fontSize: 72, margingTop: 4 }} color="alt" />
-        </CardActions>
+        <CardActions></CardActions>
       </Card>
       <CreateCollectionDialog />
       <CreateNftDialog />
