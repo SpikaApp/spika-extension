92996e6a3d0c947ac90175eb67e28beecf2769ce
xorgal <93151521+xorgal@users.noreply.github.com>
2022-07-31 16:22:40 +0300

    build file names no hash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
     module: "commonjs",
     target: "es2020",
     chunkSizeWarningLimit: 1024,
