153040cfab9fcca3fbb9fab504a8798e9ec657c0
xorgal <93151521+xorgal@users.noreply.github.com>
2022-08-03 17:05:19 +0300

    const naming change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
-export const PROTOCOL_TYPE = window.location.protocol;
+// Rest API
 export const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+
+// Platform
+export const PLATFORM = window.location.protocol;
