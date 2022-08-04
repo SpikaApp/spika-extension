dbbfa38fbbf965cb97a3ad287a9b4c48ca0882b2
xorgal <93151521+xorgal@users.noreply.github.com>
2022-08-04 07:30:40 +0300

    v0.1.8

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
             blockchain chain_id: {chain_id}
           </Typography>
           <Typography variant="subtitle1" align="center" color="textPrimary">
-            Wallet version 0.1.7 <br />
+            Wallet version 0.1.8 <br />
             Aptos SDK version 1.2.2
             <br />
             <Link
