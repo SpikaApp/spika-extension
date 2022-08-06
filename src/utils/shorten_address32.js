65222ed6af7de1bfad08f70fcc022257bb8d241c
xorgal <93151521+xorgal@users.noreply.github.com>
2022-08-06 15:48:49 +0300

    delete unused utils

--- src/utils/shorten_address32.js
+++
@@ -1,3 +0,0 @@
-const shortenAddress32 = (address) => `${address.slice(0, 16)}...${address.slice(address.length - 16)}`;
-
-export default shortenAddress32;
--- src/utils/shorten_key.js
+++
@@ -1,3 +0,0 @@
-const shortenKey = (key) => `${key.slice(0, 2)}${key.slice(key.length - 64)}`;
-
-export default shortenKey;
