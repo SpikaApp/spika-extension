2bbfe51a6ee174a3ee2be5a3071383a6b3824889
xorgal <93151521+xorgal@users.noreply.github.com>
2022-08-06 08:36:17 +0300

    v0.2.0

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-// Rest API
 export const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
-// Platform
 export const PLATFORM = window.location.protocol;
