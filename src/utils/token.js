72a006d9a3edad3af6519c526ec13d9a3a3d24bc
xorgal <93151521+xorgal@users.noreply.github.com>
2022-08-06 15:27:31 +0300

    token name, ticker

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,8 +1,14 @@
-const AptosCoin = {
-  address: "0x1",
-  module: "coin",
-  name: "CoinStore",
-  generic_type_params: ["0x1::aptos_coin::AptosCoin"],
-};
+const AptosCoin = [
+  {
+    name: "AptosCoin",
+    ticker: "APTOS",
+  },
+  {
+    address: "0x1",
+    module: "coin",
+    name: "CoinStore",
+    generic_type_params: ["0x1::aptos_coin::AptosCoin"],
+  },
+];
 
 export { AptosCoin };
