0e8fdf69f98b2cd9cf3f302900041be8a5f775cb
xorgal <93151521+xorgal@users.noreply.github.com>
2022-08-08 16:59:04 +0300

    client.js

---
+++ src/utils/client.js
@@ -0,0 +1,6 @@
+import { AptosClient, TokenClient, FaucetClient } from "aptos";
+import { NODE_URL, FAUCET_URL } from "./constants";
+
+export const client = new AptosClient(NODE_URL);
+export const tokenClient = new TokenClient(client);
+export const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL, null);
