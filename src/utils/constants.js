b089766756e6fdfa705f33d7137a17e38f87e054
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-06 04:38:28 +0300

    add extension version as constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
+import { version, dependencies } from "../../package.json";
+
+// Extension version
+export const EXTENSION_VERSION = version;
+export const APTOS_SDK_VERSION = dependencies.aptos.slice(1);
+
 // Nodes URL
 export const NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
