0e8dd5079490a29535c4f989e19d8deaa595ec89
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-07 15:12:10 +0300

    v0.3.16

--- vite.config.js
+++ vite.config.js
@@ -29,5 +29,5 @@ export default defineConfig({
     target: "es2020",
     chunkSizeWarningLimit: 1024,
   },
-  plugins: [react({ fastRefresh: true })],
+  plugins: [react({ fastRefresh: false })],
 });
