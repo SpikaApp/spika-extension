9bcc7378987d52d7da7e47b7f467de228d263f42
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-09 23:05:40 +0300

    Add asset alerts

--- src/components/AddAssetDialog.jsx
+++ src/components/AddAssetDialog.jsx
@@ -13,25 +13,42 @@ import {
   ListItemText,
   TextField,
   Paper,
+  Typography,
 } from "@mui/material";
+import Loading from "../components/Loading";
 import pixel_coin from "../assets/pixel_coin.png";
 import { UIContext } from "../context/UIContext";
 import { AccountContext } from "../context/AccountContext";
 import { Web3Context } from "../context/Web3Context";
-import coin, { coinStore, coinInfo } from "../lib/coin";
+import { coinList, coinStore, coinInfo } from "../lib/coin";
 import { client } from "../lib/client";
 import { setStore } from "../lib/store";
-import { setAsset } from "../lib/asset_store";
+import { setAsset, getAsset } from "../lib/asset_store";
 import { PLATFORM } from "../utils/constants";
+import { register } from "../lib/payload";
 
 const AddAssetDialog = () => {
   const { openAddAssetDialog, setOpenAddAssetDialog, darkMode } = useContext(UIContext);
   const { setIsLoading, currentAddress, currentAsset, setCurrentAsset, throwAlert } =
     useContext(AccountContext);
-  const { getBalance } = useContext(Web3Context);
+  const {
+    getBalance,
+    findAsset,
+    isValidAsset,
+    setIsValidAsset,
+    selectedAsset,
+    setSelectedAsset,
+    estimateTransaction,
+    isValidTransaction,
+    setIsValidTransaction,
+    estimatedTxnResult,
+    setEstimatedTxnResult,
+    registerAsset,
+  } = useContext(Web3Context);
   const [selectedIndex, setSelectedIndex] = useState("");
-  const [selectedAsset, setSelectedAsset] = useState([]);
   const [isCustomToken, setIsCustomToken] = useState(false);
+  const [estimationRequired, setEstimationRequired] = useState(false);
+  const [registerAssetRequired, setRegisterAssetRequired] = useState(false);
   const [coinType, setCoinType] = useState("");
 
   const _currentAsset = "currentAsset";
@@ -39,7 +56,56 @@ const AddAssetDialog = () => {
   const handleListItemClick = (event, index, asset) => {
     setSelectedIndex(index);
     setSelectedAsset(asset);
+    setCoinType(asset.type);
+    setEstimationRequired(true);
   };
+
+  useEffect(() => {
+    if (estimationRequired || isValidAsset) {
+      handleEstimateTransaction(coinType);
+      setEstimationRequired(false);
+    }
+  }, [estimationRequired || isValidAsset]);
+
+  //  useEffect(() => {
+  //    if (isCustomToken && isValidAsset) {
+  //      handleEstimateTransaction(coinType);
+  //      setEstimationRequired(false);
+  //    }
+  //  }, [isCustomToken && isValidAsset]);
+
+  useEffect(() => {
+    if (registerAssetRequired && openAddAssetDialog) {
+      try {
+        setIsLoading(true);
+        if (isValidTransaction && isCustomToken) {
+          setIsLoading(true);
+          registerAsset(coinType);
+          setCurrentAsset(selectedAsset);
+          setAsset(currentAddress, selectedAsset);
+          setStore(PLATFORM, _currentAsset, selectedAsset);
+          setIsLoading(false);
+          throwAlert(
+            102,
+            "Success",
+            `New asset ${selectedAsset.data.name} successfully registered.`
+          );
+        }
+        if (isValidTransaction && !isCustomToken) {
+          registerAsset(coinType);
+          setAsset(currentAddress, selectedAsset);
+          setCurrentAsset(selectedAsset);
+          setStore(PLATFORM, _currentAsset, selectedAsset);
+          throwAlert(101, "Success", `New asset ${selectedAsset.data.name} successfully added.`);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        throwAlert(103, "Error", `${error}`);
+      }
+      setRegisterAssetRequired(false);
+    }
+  }, [isValidTransaction]);
 
   useEffect(() => {
     handleGetBalance();
@@ -52,48 +118,62 @@ const AddAssetDialog = () => {
   };
 
   const handleAddCustomToken = async () => {
+    clearDialog();
     setIsCustomToken(true);
   };
 
-  const handleAddAsset = async () => {
-    if (isCustomToken) {
-      try {
-        const _address = coinType.split("::")[0];
-        const custom = await client.getAccountResource(_address, coinInfo(coinType));
-        const result = {
-          type: coinType,
-          data: {
-            name: `${custom.data.name}`,
-            symbol: `${custom.data.symbol}`,
-            decimals: custom.data.decimals,
-            logo: pixel_coin,
-            logo_alt: pixel_coin,
-          },
-        };
-        setCurrentAsset(result);
-        setAsset(currentAddress, result);
-        setStore(PLATFORM, _currentAsset, result);
-        handleCancel();
-        throwAlert(102, "Success", `New custom asset ${result.data.name} successfully added.`);
-      } catch (error) {
-        console.log(error);
-        throwAlert(103, "Error", `${error}`);
+  const handleFindAsset = async () => {
+    setIsLoading(true);
+    const data = await findAsset(coinType);
+    if (!data) {
+      throwAlert(112, "Error", "Asset not found on chain.");
+    }
+    setIsLoading(false);
+  };
+
+  const handleEstimateTransaction = async (coinType) => {
+    const payload = await register(coinType);
+    await estimateTransaction(payload, true, true);
+  };
+
+  const handleRegisterAsset = async () => {
+    if (selectedIndex !== "" || isCustomToken) {
+      const assetData = await findAsset(coinType);
+      if (assetData) {
+        const assetRegistered = await findAssetInCurrentAccount(coinType);
+        if (!assetRegistered) {
+          setEstimationRequired(true);
+          setRegisterAssetRequired(true);
+          console.log("Asset not found. Estimating gas...");
+        } else {
+          const assetInStore = getAsset(currentAddress, coinType);
+          if (!assetInStore) {
+            setCurrentAsset(selectedAsset);
+            setStore(PLATFORM, _currentAsset, selectedAsset);
+            handleCancel();
+            console.log("Asset already registered. Saving in wallet's store.");
+          } else {
+            console.log("Asset already registered and already saved in wallet's store.");
+            handleCancel();
+          }
+        }
+      } else {
+        throwAlert(112, "Error", "Asset not found on chain.");
       }
     } else {
-      if (selectedIndex !== "") {
-        setAsset(currentAddress, selectedAsset);
-        setCurrentAsset(selectedAsset);
-        setStore(PLATFORM, _currentAsset, selectedAsset);
-        handleCancel();
-        throwAlert(101, "Success", `New asset ${selectedAsset.data.name} successfully added.`);
-      } else {
-        throwAlert(104, "Error", "Select asset from the list or add custom token to continue.");
-      }
+      throwAlert(104, "Error", "Select asset from the list or add custom token to continue.");
     }
   };
 
   const handleCancel = () => {
     setOpenAddAssetDialog(false);
+    clearDialog();
+  };
+
+  const clearDialog = () => {
+    setIsValidAsset(false);
+    setIsValidTransaction(false);
+    setEstimatedTxnResult([]);
     setSelectedIndex("");
     setSelectedAsset([]);
     setIsCustomToken(false);
@@ -119,23 +199,62 @@ const AddAssetDialog = () => {
                 justifyContent: "center",
               }}
             >
-              <TextField
-                sx={{ mt: 1, mr: 2, ml: 2, width: 260 }}
-                InputLabelProps={{ shrink: true }}
-                multiline
-                rows={3}
-                label="Asset type"
-                placeholder="0x1::aptos_coin::AptosCoin"
-                autoFocus={false}
-                value={coinType}
-                onChange={(e) => setCoinType(e.target.value)}
-              />
+              {!isValidAsset ? (
+                <TextField
+                  sx={{ mt: 1, mr: 2, ml: 2, width: 260 }}
+                  InputLabelProps={{ shrink: true }}
+                  multiline
+                  rows={4}
+                  label="Asset type"
+                  placeholder="0x1::aptos_coin::AptosCoin"
+                  autoFocus={false}
+                  value={coinType}
+                  onChange={(e) => setCoinType(e.target.value)}
+                />
+              ) : (
+                <Stack>
+                  <TextField
+                    sx={{ mt: 1, mr: 2, ml: 2, width: 260 }}
+                    InputLabelProps={{ shrink: true }}
+                    multiline
+                    rows={4}
+                    label="Asset type"
+                    autoFocus={false}
+                    value={selectedAsset.type}
+                    disabled
+                  />
+                  <TextField
+                    sx={{ mt: 2, mr: 2, ml: 2, width: 260 }}
+                    InputLabelProps={{ shrink: true }}
+                    label="Name"
+                    autoFocus={false}
+                    value={selectedAsset.data.name}
+                    disabled
+                  />
+                  <TextField
+                    sx={{ mt: 2, mr: 2, ml: 2, width: 130 }}
+                    InputLabelProps={{ shrink: true }}
+                    label="Symbol"
+                    autoFocus={false}
+                    value={selectedAsset.data.symbol}
+                    disabled
+                  />
+                </Stack>
+              )}
             </Stack>
           </form>
         ) : (
-          <Paper sx={{ width: "260px", bgcolor: "background.paper" }}>
-            <List component="nav">
-              {coin.map((asset) => (
+          <Paper
+            sx={{
+              width: "260px",
+              bgcolor: "background.paper",
+            }}
+          >
+            <List
+              component="nav"
+              sx={{ overflow: "hidden", overflowY: "visible", maxHeight: "255px" }}
+            >
+              {coinList.map((asset) => (
                 <Stack key={asset.type}>
                   <ListItemButton
                     selected={selectedIndex === asset.data.name}
@@ -155,40 +274,138 @@ const AddAssetDialog = () => {
             </List>
           </Paper>
         )}
+        {!isCustomToken && (
+          <Stack
+            sx={{
+              display: "flex",
+
+              alignItems: "center",
+              justifyContent: "center",
+              mt: 1,
+            }}
+          >
+            <Button
+              variant="outlined"
+              sx={{ mt: 1, mb: 1, width: "180px" }}
+              onClick={handleAddCustomToken}
+            >
+              Add Custom Token
+            </Button>
+          </Stack>
+        )}
+        <Box
+          sx={{
+            // backgroundColor: "#F19223",
+            minHeight: "24px",
+            alignSelf: "center",
+            mt: "4px",
+          }}
+        >
+          {isValidTransaction && (
+            <Typography
+              variant="subtitle2"
+              color="warning.dark"
+              sx={{
+                maxWidth: "260px",
+              }}
+            >
+              Estimated network fee: {estimatedTxnResult.gas_used}
+            </Typography>
+          )}
+          {isCustomToken && isValidAsset && !isValidTransaction && (
+            <Stack>
+              {estimatedTxnResult && (
+                <Typography
+                  variant="subtitle2"
+                  color="error.dark"
+                  sx={{
+                    maxWidth: "260px",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    wordWrap: "break-word",
+                    mt: 1,
+                  }}
+                >
+                  {estimatedTxnResult.vm_status}
+                </Typography>
+              )}
+            </Stack>
+          )}
+        </Box>
       </DialogContent>
       <DialogActions sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
         <Stack>
-          {!isCustomToken && (
-            <Stack
-              sx={{
-                display: "flex",
-
-                alignItems: "center",
-                justifyContent: "center",
-                mt: 2,
-              }}
-            >
-              <Button onClick={handleAddCustomToken}>Add Custom Token</Button>
-            </Stack>
-          )}
-          <Stack sx={{ display: "flex", flexDirection: "row", alignItems: "center", mt: 2, mb: 2 }}>
+          <Stack sx={{ display: "flex", flexDirection: "row", alignItems: "center", mb: 2 }}>
             <Button sx={{ width: "115px" }} variant="outlined" onClick={handleCancel}>
               Cancel
             </Button>
-            <Button
-              sx={{
-                background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
-                width: "115px",
-                ml: 4,
-              }}
-              variant="contained"
-              onClick={handleAddAsset}
-            >
-              Add
-            </Button>
+            {isCustomToken && !isValidAsset && !isValidTransaction && (
+              <Button
+                sx={{
+                  background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                  width: "115px",
+                  ml: 4,
+                }}
+                variant="contained"
+                onClick={handleFindAsset}
+              >
+                Find
+              </Button>
+            )}
+            {isCustomToken && isValidAsset && !isValidTransaction && (
+              <Button
+                sx={{
+                  width: "115px",
+                  ml: 4,
+                }}
+                variant="contained"
+                disabled
+              >
+                Register
+              </Button>
+            )}
+            {isCustomToken && isValidAsset && isValidTransaction && (
+              <Button
+                sx={{
+                  background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                  width: "115px",
+                  ml: 4,
+                }}
+                variant="contained"
+                onClick={handleRegisterAsset}
+              >
+                Import
+              </Button>
+            )}
+            {!isCustomToken && isValidTransaction && (
+              <Button
+                sx={{
+                  background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                  width: "115px",
+                  ml: 4,
+                }}
+                variant="contained"
+                onClick={handleRegisterAsset}
+              >
+                Register
+              </Button>
+            )}
+            {!isCustomToken && !isValidTransaction && (
+              <Button
+                sx={{
+                  width: "115px",
+                  ml: 4,
+                }}
+                variant="contained"
+                disabled
+              >
+                Register
+              </Button>
+            )}
           </Stack>
         </Stack>
       </DialogActions>
+      <Loading />
     </Dialog>
   );
 };
