a25cc6a8b77ea174fea22b5d561cca58380622ac
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-11 00:36:25 +0300

    add pwd functions

---
+++ src/utils/pwd.js
@@ -0,0 +1,25 @@
+import { v5 as uuidv5 } from "uuid";
+import * as passworder from "@metamask/browser-passworder";
+
+const appKey = import.meta.env.VITE_SPIKA_KEY;
+const appRandom = import.meta.env.VITE_SPIKA_RANDOM;
+
+export const encryptPassword = async (pwd) => {
+  const timestamp = Date.now();
+  const key = (timestamp + appRandom).toString();
+  const secret = uuidv5(key, appKey);
+  const encryptedPassword = await passworder.encrypt(secret, pwd);
+  const result = {
+    data: encryptedPassword,
+    id: timestamp,
+  };
+  return result;
+};
+
+export const decryptPassword = async (pwd) => {
+  const timestamp = pwd.id;
+  const key = (timestamp + appRandom).toString();
+  const secret = uuidv5(key, appKey);
+  const decryptedPassword = await passworder.decrypt(secret, pwd.data);
+  return decryptedPassword;
+};
