dd20993c5f2c5bf6fabb38c86b50c46feef479f7
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-12 01:14:15 +0300

    validate txn before submitting on chain

--- src/components/CreateCollectionDialog.jsx
+++ src/components/CreateCollectionDialog.jsx
@@ -1,27 +1,77 @@
-import { useContext } from "react";
-import { Button, Dialog, DialogContent, DialogTitle, Stack, TextField } from "@mui/material";
+import { useContext, useState, useEffect } from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  TextField,
+  Box,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import Loading from "./Loading";
 import AlertDialog from "./AlertDialog";
 import { UIContext } from "../context/UIContext";
 import { Web3Context } from "../context/Web3Context";
+import { collection } from "../lib/payload";
 
 const CreateCollectionDialog = () => {
   const { openCreateCollectionDialog, setOpenCreateCollectionDialog } = useContext(UIContext);
   const {
-    collectionName,
-    setCollectionName,
-    collectionDescription,
-    setCollectionDescription,
-    collectionUri,
-    setCollectionUri,
-    handleCreateCollection,
+    estimateTransaction,
+    createToken,
+    isValidTransaction,
+    estimatedTxnResult,
+    clearPrevEstimation,
   } = useContext(Web3Context);
 
+  const [estimationRequired, setEstimationRequired] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [collectionName, setCollectionName] = useState("");
+  const [collectionDescription, setCollectionDescription] = useState("");
+  const [collectionUri, setCollectionUri] = useState("");
+
+  useEffect(() => {
+    if (estimationRequired) {
+      handleEstimateCollection();
+      setEstimationRequired(false);
+    }
+  }, [estimationRequired]);
+
+  const handleEstimationRequired = async () => {
+    setEstimationRequired(true);
+  };
+
+  const collectionPayload = async () => {
+    const payload = await collection(collectionName, collectionDescription, collectionUri, 1);
+    return payload;
+  };
+
+  const handleEstimateCollection = async () => {
+    setIsLoading(true);
+    const payload = await collectionPayload();
+    await estimateTransaction(payload, true, true);
+    setIsLoading(false);
+  };
+
+  const handleCreateCollection = async () => {
+    const payload = await collectionPayload();
+    await createToken(payload);
+    handleCancel();
+  };
+
   const handleCancel = () => {
+    setOpenCreateCollectionDialog(false);
+    clearPrevEstimation();
+    clearCollection();
+  };
+
+  const clearCollection = async () => {
     setCollectionName("");
     setCollectionDescription("");
     setCollectionUri("");
-    setOpenCreateCollectionDialog(false);
   };
 
   return (
@@ -45,6 +95,7 @@ const CreateCollectionDialog = () => {
             id="collectionName"
             label="Collection Name"
             type="string"
+            disabled={isValidTransaction ? true : false}
             value={collectionName}
             onChange={(e) => setCollectionName(e.target.value)}
           />
@@ -53,6 +104,7 @@ const CreateCollectionDialog = () => {
             id="collectionDescription"
             label="Collection Description"
             type="string"
+            disabled={isValidTransaction ? true : false}
             multiline
             rows={3}
             value={collectionDescription}
@@ -63,10 +115,58 @@ const CreateCollectionDialog = () => {
             id="collectionUri"
             label="URL"
             type="string"
+            disabled={isValidTransaction ? true : false}
             value={collectionUri}
             onChange={(e) => setCollectionUri(e.target.value)}
           />
         </Stack>
+        <Box
+          sx={{
+            // backgroundColor: "background.paper",
+            minHeight: "24px",
+            alignSelf: "center",
+            justifyContent: "center",
+            mt: "4px",
+            maxWidth: "275x",
+          }}
+        >
+          {isValidTransaction && (
+            <Typography
+              noWrap
+              align="center"
+              variant="subtitle2"
+              color="warning.dark"
+              sx={{
+                maxWidth: "260px",
+                mt: "2px",
+                ml: "12px",
+                mr: "12px",
+              }}
+            >
+              Estimated network fee: {estimatedTxnResult.gas_used}
+            </Typography>
+          )}
+
+          <Stack>
+            {estimatedTxnResult && !isValidTransaction && (
+              <Typography
+                variant="subtitle2"
+                color="error.dark"
+                sx={{
+                  maxWidth: "260px",
+                  overflow: "hidden",
+                  overflowY: "scroll",
+                  textOverflow: "ellipsis",
+                  wordWrap: "break-word",
+                  maxHeight: "48px",
+                  mt: "2px",
+                }}
+              >
+                {estimatedTxnResult.vm_status}
+              </Typography>
+            )}
+          </Stack>
+        </Box>
         <Stack
           sx={{
             display: "flex",
@@ -80,16 +180,49 @@ const CreateCollectionDialog = () => {
           <Button variant="outlined" sx={{ width: "121px", mr: 4 }} onClick={handleCancel}>
             Cancel
           </Button>
-          <Button
-            sx={{
-              background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
-              width: "121px",
-            }}
-            variant="contained"
-            onClick={handleCreateCollection}
-          >
-            Create
-          </Button>
+          {!isValidTransaction &&
+            collectionName !== "" &&
+            collectionDescription !== "" &&
+            collectionUri !== "" && (
+              <LoadingButton
+                sx={{
+                  background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                  width: "121px",
+                }}
+                variant="contained"
+                loadingIndicator={<CircularProgress sx={{ color: "#FFFFFFF" }} size={18} />}
+                loading={isLoading}
+                onClick={handleEstimationRequired}
+              >
+                Estimate
+              </LoadingButton>
+            )}{" "}
+          {!isValidTransaction &&
+            (collectionName === "" || collectionDescription === "" || collectionUri === "") && (
+              <Button
+                sx={{
+                  width: "121px",
+                }}
+                variant="contained"
+                disabled
+              >
+                Estimate
+              </Button>
+            )}
+          {isValidTransaction && (
+            <LoadingButton
+              sx={{
+                background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                width: "121px",
+              }}
+              variant="contained"
+              loadingIndicator={<CircularProgress sx={{ color: "#FFFFFFF" }} size={18} />}
+              loading={isLoading}
+              onClick={handleCreateCollection}
+            >
+              Create
+            </LoadingButton>
+          )}
         </Stack>
       </DialogContent>
       <Loading />
--- src/components/CreateNftDialog.jsx
+++ src/components/CreateNftDialog.jsx
@@ -1,33 +1,87 @@
-import { useContext } from "react";
-import { Button, Dialog, DialogContent, DialogTitle, Stack, TextField } from "@mui/material";
+import { useContext, useState, useEffect } from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  TextField,
+  Box,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import Loading from "./Loading";
 import AlertDialog from "./AlertDialog";
 import { UIContext } from "../context/UIContext";
+import { AccountContext } from "../context/AccountContext";
 import { Web3Context } from "../context/Web3Context";
+import { nft } from "../lib/payload";
 
 const CreateNftDialog = () => {
   const { openCreateNftDialog, setOpenCreateNftDialog } = useContext(UIContext);
+  const { currentAddress } = useContext(AccountContext);
   const {
-    collectionName,
-    setCollectionName,
-    nftName,
-    setNftName,
-    nftDescription,
-    setNftDescription,
-    nftUri,
-    setNftUri,
-    nftSupply,
-    setNftSupply,
-    handleCreateNft,
+    estimateTransaction,
+    isValidTransaction,
+    estimatedTxnResult,
+    clearPrevEstimation,
+    createToken,
   } = useContext(Web3Context);
+  const [isLoading, setIsLoading] = useState(false);
+  const [estimationRequired, setEstimationRequired] = useState(false);
+  const [collectionName, setCollectionName] = useState("");
+  const [nftName, setNftName] = useState("");
+  const [nftDescription, setNftDescription] = useState("");
+  const [nftUri, setNftUri] = useState("");
 
-  const handleCancel = () => {
+  useEffect(() => {
+    if (estimationRequired) {
+      handleEstimateNft();
+      setEstimationRequired(false);
+    }
+  }, [estimationRequired]);
+
+  const handleEstimationRequired = async () => {
+    setEstimationRequired(true);
+  };
+
+  const nftPayload = async () => {
+    const payload = await nft(
+      currentAddress,
+      collectionName,
+      nftName,
+      nftDescription,
+      1,
+      nftUri,
+      1
+    );
+    return payload;
+  };
+
+  const handleEstimateNft = async () => {
+    setIsLoading(true);
+    const payload = await nftPayload();
+    await estimateTransaction(payload, true, true);
+    setIsLoading(false);
+  };
+
+  const handleCreateNft = async () => {
+    const payload = await nftPayload();
+    await createToken(payload);
+    handleCancel();
+  };
+
+  const clearNftData = () => {
     setCollectionName("");
     setNftName("");
     setNftDescription("");
-    setNftSupply("");
     setNftUri("");
+  };
+  const handleCancel = () => {
     setOpenCreateNftDialog(false);
+    clearPrevEstimation();
+    clearNftData();
   };
 
   return (
@@ -68,21 +122,9 @@ const CreateNftDialog = () => {
             label="Description"
             type="string"
             multiline
-            rows={1}
+            rows={2}
             value={nftDescription}
             onChange={(e) => setNftDescription(e.target.value)}
-          />
-          <TextField
-            sx={{
-              mt: 1.5,
-              mb: 1.5,
-              width: "275px",
-            }}
-            id="nftSupply"
-            label="Editions"
-            type="number"
-            value={nftSupply}
-            onChange={(e) => setNftSupply(e.target.value)}
           />
           <TextField
             sx={{ mt: 1.5, width: "275px" }}
@@ -93,6 +135,53 @@ const CreateNftDialog = () => {
             onChange={(e) => setNftUri(e.target.value)}
           />
         </Stack>
+        <Box
+          sx={{
+            // backgroundColor: "background.paper",
+            minHeight: "24px",
+            alignSelf: "center",
+            justifyContent: "center",
+            mt: "4px",
+            maxWidth: "275x",
+          }}
+        >
+          {isValidTransaction && (
+            <Typography
+              noWrap
+              align="center"
+              variant="subtitle2"
+              color="warning.dark"
+              sx={{
+                maxWidth: "260px",
+                mt: "2px",
+                ml: "12px",
+                mr: "12px",
+              }}
+            >
+              Estimated network fee: {estimatedTxnResult.gas_used}
+            </Typography>
+          )}
+
+          <Stack>
+            {estimatedTxnResult && !isValidTransaction && (
+              <Typography
+                variant="subtitle2"
+                color="error.dark"
+                sx={{
+                  maxWidth: "260px",
+                  overflow: "hidden",
+                  overflowY: "scroll",
+                  textOverflow: "ellipsis",
+                  wordWrap: "break-word",
+                  maxHeight: "48px",
+                  mt: "2px",
+                }}
+              >
+                {estimatedTxnResult.vm_status}
+              </Typography>
+            )}
+          </Stack>
+        </Box>
         <Stack
           sx={{
             display: "flex",
@@ -106,16 +195,50 @@ const CreateNftDialog = () => {
           <Button sx={{ width: "121px", mr: 4 }} variant="outlined" onClick={handleCancel}>
             Cancel
           </Button>
-          <Button
-            sx={{
-              background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
-              width: "121px",
-            }}
-            variant="contained"
-            onClick={handleCreateNft}
-          >
-            Create
-          </Button>
+          {!isValidTransaction &&
+            collectionName !== "" &&
+            nftName !== "" &&
+            nftDescription !== "" &&
+            nftUri !== "" && (
+              <LoadingButton
+                sx={{
+                  background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                  width: "121px",
+                }}
+                variant="contained"
+                loadingIndicator={<CircularProgress sx={{ color: "#FFFFFFF" }} size={18} />}
+                loading={isLoading}
+                onClick={handleEstimationRequired}
+              >
+                Estimate
+              </LoadingButton>
+            )}{" "}
+          {!isValidTransaction &&
+            (collectionName === "" || nftName === "" || nftDescription === "" || nftUri === "") && (
+              <Button
+                sx={{
+                  width: "121px",
+                }}
+                variant="contained"
+                disabled
+              >
+                Estimate
+              </Button>
+            )}
+          {isValidTransaction && (
+            <LoadingButton
+              sx={{
+                background: "linear-gradient(126.53deg, #3FE1FF -25.78%, #1700FF 74.22%);",
+                width: "121px",
+              }}
+              variant="contained"
+              loadingIndicator={<CircularProgress sx={{ color: "#FFFFFFF" }} size={18} />}
+              loading={isLoading}
+              onClick={handleCreateNft}
+            >
+              Create
+            </LoadingButton>
+          )}
         </Stack>
       </DialogContent>
       <Loading />
