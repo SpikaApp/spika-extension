50d4b5f0206d2598b4bc03548c216ea05616969a
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-14 23:40:42 +0300

    comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,9 +6,10 @@ export const APTOS_SDK_VERSION = dependencies.aptos.slice(1);
 
 // Nodes URL
 export const NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+// export const NODE_URL = "https://testnet.aptoslabs.com/v1";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
-// BIP32 Aptos derivative path
+// BIP44 Aptos derivative path
 export const APTOS_DERIVE_PATH = "m/44'/637'/0'/0'/0'";
 
 // Extension type
