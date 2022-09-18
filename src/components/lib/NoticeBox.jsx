9dd5fdc565f3f06750a1099f13069076f8ec4ec1
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-18 17:48:35 +0300

    Add reusable components

---
+++ src/components/lib/NoticeBox.jsx
@@ -0,0 +1,23 @@
+import { Box, DialogContent, Typography, Stack } from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
+
+const NoticeBox = (props) => {
+  return (
+    <Box component={DialogContent} sx={{ mt: props.mt, border: 2, borderColor: "warning.light" }}>
+      <Stack
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          mt: "-12px",
+          mb: "-12px",
+        }}
+      >
+        <WarningIcon color="warning" sx={{ fontSize: "24px", ml: "-12px", mr: "12px" }} />
+        <Typography align="left">{props.text}</Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default NoticeBox;
