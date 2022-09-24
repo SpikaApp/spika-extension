c2fec78d2d0d884cf54790e2981a81165de0f726
xorgal <93151521+xorgal@users.noreply.github.com>
2022-09-24 05:17:38 +0300

    argo_usd

--- src/lib/coin.js
+++ src/lib/coin.js
@@ -1,5 +1,6 @@
 import aptos_light from "../assets/aptos_light.png";
 import aptos_dark from "../assets/aptos_dark.png";
+// import argo_usd from "../assets/argo_usd.svg";
 
 export const aptosCoin = {
   type: "0x1::aptos_coin::AptosCoin",
@@ -102,6 +103,17 @@ export const coinList = [
       swap: false,
     },
   },
+  // {
+  //   type: "0x1000000f373eb95323f8f73af0e324427ca579541e3b70c0df15c493c72171aa::usda::USDA",
+  //   data: {
+  //     name: "Argo USD",
+  //     symbol: "USDA",
+  //     decimals: 6,
+  //     logo: argo_usd,
+  //     logo_alt: argo_usd,
+  //     swap: false,
+  //   },
+  // },
 ];
 
 export const coinStore = (type) => {
