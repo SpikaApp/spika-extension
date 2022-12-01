974c6ee31c1c1f9559a5eb1d9006e4b30298c06d
xorgal <93151521+xorgal@users.noreply.github.com>
2022-12-02 01:14:59 +0300

    Disable React StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UIProvider } from "./context/UIContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <UIProvider>
-      <App />
-    </UIProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <UIProvider>
+    <App />
+  </UIProvider>
+  // </React.StrictMode>
 );
