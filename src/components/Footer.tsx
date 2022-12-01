060c9f724f4fb554f8f2420356c6904efafa42f7
xorgal <93151521+xorgal@users.noreply.github.com>
2022-12-02 01:12:41 +0300

    Open Explorer in selected network

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,40 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { Link, Typography } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AccountContext } from "../context/AccountContext";
 
+type IExplorerNetwork = "mainnet" | "testnet" | "devnet";
+
 const Footer = (): JSX.Element => {
-  const { currentAddress } = useContext(AccountContext);
+  const { currentAddress, currentNetwork } = useContext(AccountContext);
+  const [network, setNetwork] = useState<IExplorerNetwork>("mainnet");
+
+  useEffect(() => {
+    if (currentNetwork) {
+      const networkString = getNetworkString();
+      setNetwork(networkString);
+    }
+  }, [currentNetwork]);
+
+  const getNetworkString = () => {
+    switch (currentNetwork!.name) {
+      case "Mainnet":
+        return "mainnet";
+      case "Testnet":
+        return "testnet";
+      case "Devnet":
+        return "devnet";
+      default:
+        return "mainnet";
+    }
+  };
+
   return (
     <Typography sx={{ mt: 2 }} variant="subtitle1" align="center" color="textPrimary">
       View account in{" "}
       <Link
-        href={`https://explorer.devnet.aptos.dev/account/${currentAddress}`}
+        href={`https://explorer.devnet.aptos.dev/account/${currentAddress}?network=${network}`}
         underline="none"
         target="_blank"
         color="link"
