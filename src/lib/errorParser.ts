4abca96e3e574938a436037ec9bf45a25664a880
xorgal <93151521+xorgal@users.noreply.github.com>
2022-12-15 05:00:59 +0300

    Better error handling

--- src/lib/errorParser.ts
+++ src/lib/errorParser.ts
@@ -1,6 +1,20 @@
+import { ApiError } from "aptos";
 const errorParser = (error: unknown, defaultResponse: string): string => {
   let response = defaultResponse;
-  if (error instanceof Error) {
+  if (error instanceof ApiError) {
+    if (error.message) {
+      try {
+        const maybeObject = JSON.parse(error.message);
+        if (maybeObject && maybeObject.message) {
+          response = maybeObject.message;
+        } else {
+          response = error.message;
+        }
+      } catch (parsingError) {
+        response = error.message;
+      }
+    }
+  } else if (error instanceof Error) {
     response = error.message;
   }
   return response;
