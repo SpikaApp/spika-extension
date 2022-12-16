359d354ab02ff154b73d6e72e214e468d6d229f2
xorgal <93151521+xorgal@users.noreply.github.com>
2022-12-16 13:59:21 +0300

    Return "N/A" by default as txn function

--- src/utils/getTxnFunction.ts
+++ src/utils/getTxnFunction.ts
@@ -1,9 +1,13 @@
 const getTxnFunction = (functionString: string): string => {
-  const data = functionString.split("::");
-  if (data.length === 3) {
-    return `${data[1]} ${data[2]}`;
-  } else {
-    return functionString;
+  try {
+    const data = functionString.split("::");
+    if (data.length === 3) {
+      return `${data[1]} ${data[2]}`;
+    } else {
+      return functionString;
+    }
+  } catch (error) {
+    return "N/A";
   }
 };
 
