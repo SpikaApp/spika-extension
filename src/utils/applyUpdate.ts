8176f41d091e40b3a162e9284904c2b37a6bdab8
xorgal <93151521+xorgal@users.noreply.github.com>
2022-12-16 22:25:12 +0300

    Fix updates not being applied

--- src/utils/applyUpdate.ts
+++ src/utils/applyUpdate.ts
@@ -10,17 +10,20 @@ const _currentNetwork = "currentNetwork";
 const _accountNetworks = "accountNetworks";
 const _pwd = "PWD";
 
-let version: string;
+const currentVersion = await getStore(PLATFORM, _accountVersion);
+let updateVersion = currentVersion;
 
 const applyUpdate = async (): Promise<void> => {
   await getVersion();
-  if (version) {
+  if (currentVersion) {
     await v0_4_0();
     await v0_4_5();
     await v0_4_14();
     await v0_4_30();
     await v0_4_34();
     await v0_5_4();
+    await v1_0_2();
+    setStore(PLATFORM, _accountVersion, updateVersion);
   }
 };
 
@@ -41,64 +44,74 @@ const pendingUpdate = (version: string, currentUpdate: string): boolean => {
 };
 
 const v0_4_0 = async (): Promise<void> => {
-  const currentUpdate = "0.4.0";
-  const required: boolean = pendingUpdate(version, currentUpdate);
+  updateVersion = "0.4.0";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
   if (required) {
     removeStore(PLATFORM, _currentAsset);
     removeStore(PLATFORM, _accountAssets);
     setVersion();
-    logUpdate(currentUpdate);
+    logUpdate(updateVersion);
   }
 };
 
 const v0_4_5 = async (): Promise<void> => {
-  const currentUpdate = "0.4.5";
-  const required: boolean = pendingUpdate(version, currentUpdate);
+  updateVersion = "0.4.5";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
   if (required) {
     removeMem(PLATFORM, _pwd);
     setVersion();
-    logUpdate(currentUpdate);
+    logUpdate(updateVersion);
   }
 };
 
 const v0_4_14 = async (): Promise<void> => {
-  const currentUpdate = "0.4.14";
-  const required: boolean = pendingUpdate(version, currentUpdate);
+  updateVersion = "0.4.14";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
   if (required) {
     removeStore(PLATFORM, _connectedApps);
     setVersion();
-    logUpdate(currentUpdate);
+    logUpdate(updateVersion);
   }
 };
 
 const v0_4_30 = async (): Promise<void> => {
-  const currentUpdate = "0.4.30";
-  const required: boolean = pendingUpdate(version, currentUpdate);
+  updateVersion = "0.4.30";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
   if (required) {
     removeStore(PLATFORM, _currentNetwork);
     removeStore(PLATFORM, _accountNetworks);
     setVersion();
-    logUpdate(currentUpdate);
+    logUpdate(updateVersion);
   }
 };
 
 const v0_4_34 = async (): Promise<void> => {
-  const currentUpdate = "0.4.34";
-  const required: boolean = pendingUpdate(version, currentUpdate);
+  updateVersion = "0.4.34";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
   if (required) {
     removeMem(PLATFORM, _pwd);
     setVersion();
-    logUpdate(currentUpdate);
+    logUpdate(updateVersion);
   }
 };
 
 const v0_5_4 = async (): Promise<void> => {
-  const currentUpdate = "0.5.4";
-  const required: boolean = pendingUpdate(version, currentUpdate);
+  updateVersion = "0.5.4";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
   if (required) {
     removeMem(PLATFORM, _pwd);
     setVersion();
-    logUpdate(currentUpdate);
+    logUpdate(updateVersion);
+  }
+};
+
+const v1_0_2 = async (): Promise<void> => {
+  updateVersion = "1.0.2";
+  const required: boolean = pendingUpdate(currentVersion, updateVersion);
+  if (required) {
+    removeMem(PLATFORM, _accountAssets);
+    setVersion();
+    logUpdate(updateVersion);
   }
 };
 
